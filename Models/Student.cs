b09bdcb4f2aa6a04995be17705ba934f532ccd77
AyushKumar <ayushaktu1234@gmail.com>
2020-12-04 16:12:59 +0530

    SecondCommit

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeeklyAssisment_04_12.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public string SDob { get; set; }
+        public string SContect { get; set; }
+    }
+}
